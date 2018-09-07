339d2418be5a11db10a0da35652e6cb9e7127594
dongshenyun <40025139+dongshenyun@users.noreply.github.com>
2018-09-07 11:03:02 +0800

    Add files via upload

---
+++ jiangzuo/wuliangshou.js
@@ -0,0 +1,140 @@
+﻿if(GetQueryString('v')==500){
+document.write(
+"<li><a href='http://player.youku.com/embed/XMTMzMTkwMjYyNA==' target='donghua'>学习无量寿经心得体会分享 第一集 (字幕版 ）（陶永吉居士）</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTMzMTkyMjQwMA==' target='donghua'>学习无量寿经心得体会分享 第二集（陶永吉居士）</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTMzMjA4MDU5Mg==' target='donghua'>学习无量寿经心得体会分享 第三集 (字幕版 ）（陶永吉居士）</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTMzMjcyOTA1Mg==' target='donghua'>学习无量寿经心得体会分享 第四集（陶永吉居士）</a></li>","<li><a href='http://player.youku.com/embed/XMTMzMjczNTE4NA==' target='donghua'>学习无量寿经心得体会分享 第五集（陶永吉居士）</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTMzMjc4NTI0NA==' target='donghua'>学习无量寿经心得体会分享 第六集（陶永吉居士）</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTMzMjc5Njg5Ng==' target='donghua'>学习无量寿经心得体会分享 第七集（陶永吉居士）</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTMzMjgyNDY5Mg==' target='donghua'>学习无量寿经心得体会分享 第八集（陶永吉居士）</a></li>"
+);
+}
+
+
+if(GetQueryString('v')==501){
+document.write(
+"<li><a href='http://player.youku.com/embed/XMTgxNzY2MzAyOA==' target='donghua'>無量壽經報恩談 123 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzY2MzAyNA==' target='donghua'>無量壽經報恩談 122 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzY2MzAzMg==' target='donghua'>無量壽經報恩談 121 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzYzODc0NA==' target='donghua'>無量壽經報恩談 120 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzYzNzYyMA==' target='donghua'>無量壽經報恩談 119 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzYzNjI5Ng==' target='donghua'>無量壽經報恩談 118 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzYxNjA1Mg==' target='donghua'>無量壽經報恩談 117 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzYxNDA3Mg==' target='donghua'>無量壽經報恩談 116 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzYxMzc2MA==' target='donghua'>無量壽經報恩談 115 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzU5NDI0MA==' target='donghua'>無量壽經報恩談 114 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzU5MjA5Mg==' target='donghua'>無量壽經報恩談 113 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzU5MTE4OA==' target='donghua'>無量壽經報恩談 112 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzU3MzA2MA==' target='donghua'>無量壽經報恩談 111 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzU1MTMwMA==' target='donghua'>無量壽經報恩談 110 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzU0OTc0MA==' target='donghua'>無量壽經報恩談 109 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzU0NDc2OA==' target='donghua'>無量壽經報恩談 108 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzUyNzAyOA==' target='donghua'>無量壽經報恩談 107 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzUyNjk5Mg==' target='donghua'>無量壽經報恩談 106 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzU2OTAxNg==' target='donghua'>無量壽經報恩談 105 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzU2ODYwOA==' target='donghua'>無量壽經報恩談 104 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzUyMTc4NA==' target='donghua'>無量壽經報恩談 103 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzUwNjY0OA==' target='donghua'>無量壽經報恩談 102 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzUwNjA2MA==' target='donghua'>無量壽經報恩談 101 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzQ5OTc5Ng==' target='donghua'>無量壽經報恩談 100 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzQ4MzY5Ng==' target='donghua'>無量壽經報恩談 099 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzQ4MTY5Ng==' target='donghua'>無量壽經報恩談 098 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNzQ3Njg1Ng==' target='donghua'>無量壽經報恩談 097 定弘法師</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTcwODcyMjg4OA==' target='donghua'>定弘法师 - 无量寿经报恩谈 95</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTcwODcyMjkwNA==' target='donghua'>定弘法师 - 无量寿经报恩谈 94</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY5ODg2NTE0NA==' target='donghua'>定弘法师 - 无量寿经报恩谈 93</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY5ODg2NTIwMA==' target='donghua'>定弘法师 - 无量寿经报恩谈 92</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY5ODg2NTMyNA==' target='donghua'>定弘法师 - 无量寿经报恩谈 91</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY5ODg2NTk4OA==' target='donghua'>定弘法师 - 无量寿经报恩谈 90</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY5ODg3NDAyNA==' target='donghua'>定弘法师 - 无量寿经报恩谈 89</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY4Njk4MjAzMg==' target='donghua'>定弘法师 - 无量寿经报恩谈 88</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY4Njk4MTk5Mg==' target='donghua'>定弘法师 - 无量寿经报恩谈 87</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3OTk3MjAzMg==' target='donghua'>定弘法师 - 无量寿经报恩谈 86</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3OTk3MjAyOA==' target='donghua'>定弘法师 - 无量寿经报恩谈 85</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY2NzAxNDY0OA==' target='donghua'>定弘法师 - 无量寿经报恩谈 84</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDQ5MzU0OA==' target='donghua'>無量壽經報恩談 083 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDQ5MzUzNg==' target='donghua'>無量壽經報恩談 082 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDQ5OTc2NA==' target='donghua'>無量壽經報恩談 081 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDUwOTE0MA==' target='donghua'>無量壽經報恩談 080 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDUwOTMwOA==' target='donghua'>無量壽經報恩談 079 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDUxMTIwNA==' target='donghua'>無量壽經報恩談 078 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDUyNDAzNg==' target='donghua'>無量壽經報恩談 077 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDUyNDQxMg==' target='donghua'>無量壽經報恩談 076 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDUyODc0NA==' target='donghua'>無量壽經報恩談 075 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDUzNTA2MA==' target='donghua'>無量壽經報恩談 074 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTY3NDUzNzg4NA==' target='donghua'>無量壽經報恩談 073 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYzOTI5MDg3Mg==' target='donghua'>定弘法师 无量寿经报恩谈 72</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzUxNjIzNg==' target='donghua'>無量壽經報恩談 071 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzUxNjIzMg==' target='donghua'>無量壽經報恩談 070 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzUxNjIyNA==' target='donghua'>無量壽經報恩談 069 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzUyMDU4MA==' target='donghua'>無量壽經報恩談 068 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzUyMTk0NA==' target='donghua'>無量壽經報恩談 067 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzUyMjIxNg==' target='donghua'>無量壽經報恩談 066 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzUyNTYzNg==' target='donghua'>無量壽經報恩談 065 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzUyNzA5Mg==' target='donghua'>無量壽經報恩談 064 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ3ODE5Ng==' target='donghua'>無量壽經報恩談 063 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ3ODE4OA==' target='donghua'>無量壽經報恩談 062 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ3ODE5Mg==' target='donghua'>無量壽經報恩談 061 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTcwODcyMjg4MA==' target='donghua'>定弘法师 - 无量寿经报恩谈 96</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ4MDQ0NA==' target='donghua'>無量壽經報恩談 060 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ4MDQ3Ng==' target='donghua'>無量壽經報恩談 059 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ4MTI2NA==' target='donghua'>無量壽經報恩談 058 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ4MjYyMA==' target='donghua'>無量壽經報恩談 057 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ4NDE0OA==' target='donghua'>無量壽經報恩談 056 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ4NjQ0OA==' target='donghua'>無量壽經報恩談 055 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ4Njk5Mg==' target='donghua'>無量壽經報恩談 054 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxNzQ4NzkwOA==' target='donghua'>無量壽經報恩談 053 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTU4MzI5OTQ2NA==' target='donghua'>無量壽經報恩談 052 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTU4MzI5OTQyNA==' target='donghua'>無量壽經報恩談 051 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTU4MjE5MDU4MA==' target='donghua'>無量壽經報恩談 050 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTU4MjE5MDU4NA==' target='donghua'>無量壽經報恩談 049 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTU4MjE5MDU0MA==' target='donghua'>無量壽經報恩談 048 定弘法師 </a></li>","<li><a href='http://player.youku.com/embed/XMTU4MjE5NzQ2OA==' target='donghua'>無量壽經報恩談 047 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTU4MjE5NzkxNg==' target='donghua'>無量壽經報恩談 046 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTU2MTczMTk0OA==' target='donghua'>無量壽經報恩談 045 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTU2MTczMTk3Ng==' target='donghua'>無量壽經報恩談 044 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTU2MTczMjAxMg==' target='donghua'>無量壽經報恩談 043 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxNjUzMTM5Mg==' target='donghua'>無量壽經報恩談 042 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxNTAzNDY0MA==' target='donghua'>無量壽經報恩談 041 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxNDk3MjY0OA==' target='donghua'>無量壽經報恩談 040 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxNDk3MjY0MA==' target='donghua'>無量壽經報恩談 039 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDYxNTQwMA==' target='donghua'>無量壽經報恩談 038 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDYxNTM4MA==' target='donghua'>無量壽經報恩談 037 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDYxNTQwOA==' target='donghua'>無量壽經報恩談 036 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDY4MzYwMA==' target='donghua'>無量壽經報恩談 035 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDY5MTE4NA==' target='donghua'>無量壽經報恩談 034 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDY5NDg2NA==' target='donghua'>無量壽經報恩談 033 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDc5ODM1Ng==' target='donghua'>無量壽經報恩談 032 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDc5ODQwOA==' target='donghua'>無量壽經報恩談 031 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDc5ODQxNg==' target='donghua'>無量壽經報恩談 030 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDkwMDgxMg==' target='donghua'>無量壽經報恩談 029 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTYxMTU4MTkzNg==' target='donghua'>定弘法师 - 无量寿经报恩谈 28</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDkwNjY4OA==' target='donghua'>無量壽經報恩談 027 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMDkxMTcwNA==' target='donghua'>無量壽經報恩談 026 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMTA3NzU1Mg==' target='donghua'>無量壽經報恩談 025 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUxMTA3OTY2OA==' target='donghua'>無量壽經報恩談 024 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzEzMDU4NA==' target='donghua'>無量壽經報恩談 023 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzEzMDY0MA==' target='donghua'>無量壽經報恩談 022 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgxNjM0NjkwNA==' target='donghua'>無量壽經報恩談 011 定弘法師</a></li>",
+
+"<li><a href='http://player.youku.com/embed/XMTQ5NzM0ODk1Ng==' target='donghua'>無量壽經報恩談 021 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzEzMDU5Ng==' target='donghua'>無量壽經報恩談 020 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzIxNjU2MA==' target='donghua'>無量壽經報恩談 019 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzIxODkwNA==' target='donghua'>無量壽經報恩談 018 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzIxOTIwNA==' target='donghua'>無量壽經報恩談 017 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzM4MDgxMg==' target='donghua'>無量壽經報恩談 016 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzIxOTg3Mg==' target='donghua'>無量壽經報恩談 015 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzM1MTYyNA==' target='donghua'>無量壽經報恩談 014 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzI2MjAyNA==' target='donghua'>無量壽經報恩談 013 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzI2MzgwOA==' target='donghua'>無量壽經報恩談 012 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzI3Mjk2NA==' target='donghua'>無量壽經報恩談 011 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUwNDczMzM1Ng==' target='donghua'>無量壽經報恩談 010 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzI5MTE1Ng==' target='donghua'>無量壽經報恩談 009 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzI5MTUyNA==' target='donghua'>無量壽經報恩談 008 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzM2NDk1Mg==' target='donghua'>無量壽經報恩談 007 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzMwNjUxMg==' target='donghua'>無量壽經報恩談 006 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzMxMzExMg==' target='donghua'>無量壽經報恩談 005 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzMxNjkwMA==' target='donghua'>無量壽經報恩談 004 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzMyNTk4OA==' target='donghua'>無量壽經報恩談 003 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTQ5NzMzNTgxNg==' target='donghua'>無量壽經報恩談 002 定弘法師 </a></li>",
+"<li><a href='http://player.youku.com/embed/XMTUwNDczMTc3Mg==' target='donghua'>無量壽經報恩談 001 定弘法師 </a></li>"
+);}
