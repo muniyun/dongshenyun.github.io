a2d7a70d65318798d0eacd7b48ca9d308751b959
dongshenyun <40025139+dongshenyun@users.noreply.github.com>
2018-09-08 13:22:56 +0800

    Add files via upload

---
+++ jiangzuo/lianju.js
@@ -0,0 +1,53 @@
+﻿if(GetQueryString('v')==10000){
+document.write("<li><a href='http://player.youku.com/embed/XNjA0ODI4ODg4' target='donghua'>释迦摩尼佛的故事1</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjA0ODMwODYw' target='donghua'>释迦摩尼佛的故事2</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjA0ODMyNDQw' target='donghua'>释迦摩尼佛的故事3</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjA0ODM1NDAw' target='donghua'>释迦摩尼佛的故事4</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjA0ODM3MzU2' target='donghua'>释迦摩尼佛的故事5</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjA0ODQwMDA4' target='donghua'>释迦摩尼佛的故事6</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjA0ODQ0NjQ4' target='donghua'>释迦摩尼佛的故事7</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjA0ODU2NjU2' target='donghua'>释迦摩尼佛的故事8</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjA0ODU4MDQw' target='donghua'>释迦摩尼佛的故事9</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjA0ODYyNTYw' target='donghua'>释迦摩尼佛的故事10</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjY1MzQ0' target='donghua'>释迦摩尼佛的故事11</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjY1Mjg0' target='donghua'>释迦摩尼佛的故事12</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjY0NDUy' target='donghua'>释迦摩尼佛的故事13</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjYzNDEy' target='donghua'>释迦摩尼佛的故事14</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjYzMzk2' target='donghua'>释迦摩尼佛的故事15</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjYyMzQ0' target='donghua'>释迦摩尼佛的故事16</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjYxNTMy' target='donghua'>释迦摩尼佛的故事17</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjYxMjA0' target='donghua'>释迦摩尼佛的故事18</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjYwMzc2' target='donghua'>释迦摩尼佛的故事19</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjU5Mjg0' target='donghua'>释迦摩尼佛的故事20</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjU5NDM2' target='donghua'>释迦摩尼佛的故事21</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjU4NDU2' target='donghua'>释迦摩尼佛的故事22</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjU3MDEy' target='donghua'>释迦摩尼佛的故事23</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjU2NTky' target='donghua'>释迦摩尼佛的故事24</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjU3MjYw' target='donghua'>释迦摩尼佛的故事25</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjU0Njcy' target='donghua'>释迦摩尼佛的故事26</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjU0NTQ4' target='donghua'>释迦摩尼佛的故事27</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjU0NTY4' target='donghua'>释迦摩尼佛的故事28</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjUyMDA4' target='donghua'>释迦摩尼佛的故事29</a></li>",
+"<li><a href='http://player.youku.com/embed/XNjAxNjUyMDI0' target='donghua'>释迦摩尼佛的故事30</a></li>",
+"<li><a href='http://player.youku.com/embed/XNTUwMTc5MTUy' target='donghua'>佛教动画释迦牟尼佛的故事 第三十一集</a></li>",
+"<li><a href='http://player.youku.com/embed/XNTUwMTg2MzE2' target='donghua'>佛教动画释迦牟尼佛的故事 第三十二集</a></li>",
+"<li><a href='http://player.youku.com/embed/XNTUwMTkyNjg0' target='donghua'>佛教动画释迦牟尼佛的故事 第三十三集</a></li>",
+"<li><a href='http://player.youku.com/embed/XNTUwMTk4OTU2' target='donghua'>佛教动画释迦牟尼佛的故事 第三十四集</a></li>",
+"<li><a href='http://player.youku.com/embed/XNTUwMjA0NjU2' target='donghua'>佛教动画释迦牟尼佛的故事 第三十五集</a></li>",
+"<li><a href='http://player.youku.com/embed/XNTUwMjE2NjI4' target='donghua'>佛教动画释迦牟尼佛的故事 第三十七集</a></li>",
+"<li><a href='http://player.youku.com/embed/XNTUwMjIyMjc2' target='donghua'>佛教动画释迦牟尼佛的故事 第三十八集</a></li>",
+"<li><a href='http://player.youku.com/embed/XNTUwMjMwNjAw' target='donghua'>佛教动画释迦牟尼佛的故事 第三十九集</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTk0NDM2NjQ3Mg==' target='donghua'>《释迦牟尼佛的故事》第40集</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTk0NDQyMjg0OA==' target='donghua'>《释迦牟尼佛的故事》第41集</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTk0NDQzNzk4NA==' target='donghua'>《释迦牟尼佛的故事》第42集</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTM4MDE1ODUwMA==' target='donghua'>《释迦牟尼佛的故事》43</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTM4MDE2NjIyOA==' target='donghua'>《释迦牟尼佛的故事》44</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTM4MDE3NDQ5Mg==' target='donghua'>《释迦牟尼佛的故事》45</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgzNjI1NzExNg==' target='donghua'>释迦牟尼佛的故事第46集</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTgzNjI2MjQyOA==' target='donghua'>释迦牟尼佛的故事第47集</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTg3MjE0NDU2NA==' target='donghua'>《释迦牟尼佛的故事》第48集</a></li>",
+"<li><a href='http://player.youku.com/embed/XMTg3MjEzNjE0NA==' target='donghua'>《释迦牟尼佛的故事》第49集</a></li>",
+"<li><a href='http://player.youku.com/embed/XMjYxNDM5NzIxMg==' target='donghua'>释迦牟尼佛的故事50集</a></li>",
+"<li><a href='http://player.youku.com/embed/XMjYxNDQwMDcxMg==' target='donghua'>释迦牟尼佛的故事第51集</a></li>"
+);}
+
