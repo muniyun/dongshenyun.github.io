73c9eb92658bfb914ad2bf4df3e6f886cac8c774
dongshenyun <40025139+dongshenyun@users.noreply.github.com>
2018-09-10 09:29:53 +0800

    Add files via upload

---
+++ fojing.js
@@ -0,0 +1,12 @@
+﻿if(GetQueryString("v")==5000){
+       
+document.getElementById("dd").innerHTML = '<object style="broder:0px;overflow:scroll;overflow-x:hidden;overflow-y:hidden" type="text/html" data="./foluen/141大愿.html" width="100%" height="100%"></object>';
+      
+
+}
+if(GetQueryString("v")==5001){
+       
+document.getElementById("dd").innerHTML = '<object style="broder:0px;overflow:scroll;overflow-x:hidden;overflow-y:hidden" type="text/html" data="./foluen/十善戒.html" width="100%" height="4000"></object>';
+      
+
+}
