0ba8fa01ee9ba0e253fa4197dd8d8612a2cdb762
dongshenyun <40025139+dongshenyun@users.noreply.github.com>
2018-10-22 17:24:28 +0800

    Update chaxun.js

--- chaxun.js
+++ chaxun.js
@@ -1,4 +1,4 @@
-﻿if(GetQueryString("c")==50000){
+if(GetQueryString("c")==50000){
 document.write("<script charset='utf-8' type='text/javascript' src='fojing3.js'></script>");}
 if(GetQueryString("c")==50001){
 document.write("<script charset='utf-8' type='text/javascript' src='wenku.js'></script><script charset='utf-8' type='text/javascript' src='wenku2.js'></script><script charset='utf-8' type='text/javascript' src='wenku3.js'></script><script charset='utf-8' type='text/javascript' src='wenku4.js'></script><script charset='utf-8' type='text/javascript' src='wenku5.js'></script>");}
