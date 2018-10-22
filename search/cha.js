7f20575c8e055055b4033129d598f2a10b10b725
dongshenyun <40025139+dongshenyun@users.noreply.github.com>
2018-10-22 17:16:56 +0800

    Update cha.js

--- search/cha.js
+++ search/cha.js
@@ -1,5 +1,5 @@
 ﻿if(GetQueryString("c")==50000){
-document.write("<script charset='utf-8' type='text/javascript' src='fojing2.js'></script><script charset='utf-8' type='text/javascript' src='fojing.js'></script>");}
+document.write("<script charset='utf-8' type='text/javascript' src='fojing3.js'></script><script charset='utf-8' type='text/javascript' src='fojing.js'></script>");}
 if(GetQueryString("c")==50001){
 document.write("<script charset='utf-8' type='text/javascript' src='wenku.js'></script><script charset='utf-8' type='text/javascript' src='wenku2.js'></script><script charset='utf-8' type='text/javascript' src='wenku3.js'></script><script charset='utf-8' type='text/javascript' src='wenku4.js'></script><script charset='utf-8' type='text/javascript' src='wenku5.js'></script>");}
 if(GetQueryString("c")==50002){
