2f9b8fd34ebff133e230db7b177754509d3744f0
dongshenyun <40025139+dongshenyun@users.noreply.github.com>
2018-10-23 19:25:14 +0800

    Delete chaxun.js

--- chaxun.js
+++
@@ -1,8 +0,0 @@
-if(GetQueryString("a")==50000){
-document.write("<script charset='utf-8' type='text/javascript' src='fojing3.js'></script>");}
-if(GetQueryString("a")==50001){
-document.write("<script charset='utf-8' type='text/javascript' src='wenku.js'></script><script charset='utf-8' type='text/javascript' src='wenku2.js'></script><script charset='utf-8' type='text/javascript' src='wenku3.js'></script><script charset='utf-8' type='text/javascript' src='wenku4.js'></script><script charset='utf-8' type='text/javascript' src='wenku5.js'></script>");}
-if(GetQueryString("a")==50002){
-document.write("<script type='text/javascript' src='yulu.js'></script>");}
-if(GetQueryString("a")==50003){
-document.write("<script type='text/javascript' src='shipin.js'></script>");}
